6cd22d666cbb0a02c5356150fbdde370bc8d85fd
Jorge Vicuña <jvicuna2001@gmail.com>
2023-09-15 15:49:49 -0300

    Implementado controlador

--- src/main/java/cl/usm/hdd/Certamen1/controllers/PeliculaController.java
+++ src/main/java/cl/usm/hdd/Certamen1/controllers/PeliculaController.java
@@ -1,2 +1,60 @@
-package cl.usm.hdd.Certamen1.controllers;public class PeliculaController {
+package cl.usm.hdd.Certamen1.controllers;
+
+import cl.usm.hdd.Certamen1.entities.Pelicula;
+import cl.usm.hdd.Certamen1.services.PeliculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+public class PeliculaController {
+    @Autowired
+    private PeliculaService peliculaService;
+
+    @GetMapping("/verPeliculas")
+    public ResponseEntity<List<Pelicula>> ver() {
+        try {
+            return ResponseEntity.ok(this.peliculaService.ver());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/crearPelicula")
+    public ResponseEntity<Pelicula> crear(@RequestBody Pelicula pelicula) {
+        try {
+            Pelicula peliculaCreada = this.peliculaService.crear(pelicula);
+            return ResponseEntity.ok(peliculaCreada);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/peliculas/{limite}")
+    public ResponseEntity<List<Pelicula>> verPorEdad(@PathVariable String limite) {
+        try {
+            List<Pelicula> peliculasPorEdad = this.peliculaService.verPorEdad(edadLimiteMapping(limite));
+            return ResponseEntity.ok(peliculasPorEdad);
+        } catch (AssertionError e) {
+            return ResponseEntity.badRequest().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    private int edadLimiteMapping(String rangoEdad) throws AssertionError{
+        switch (rangoEdad.toLowerCase()) {
+            case "menor_edad":
+                return 15;
+            case "adolescentes":
+                return 17;
+            case "mayor":   // Una persona mayor puede ver cualquier película.
+                return Integer.MAX_VALUE;
+            default:
+                throw new AssertionError("Parámetro inválido");
+        }
+    }
 }
